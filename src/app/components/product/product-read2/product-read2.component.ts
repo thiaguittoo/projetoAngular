24b9e1e0490edea19a6fa875c4aa41c796ee1eb6
Gabriellkerber <gabriellkerber@bol.com.br>
2020-05-03 15:43:36 -0300

    commit

---
+++ src/app/components/product/product-read2/product-read2.component.ts
@@ -0,0 +1,34 @@
+import { Product } from './../product.model';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTable } from '@angular/material/table';
+import { ProductRead2DataSource } from './product-read2-datasource';
+import { ProductService } from '../product.service';
+
+@Component({
+  selector: 'app-product-read2',
+  templateUrl: './product-read2.component.html',
+  styleUrls: ['./product-read2.component.css']
+})
+export class ProductRead2Component implements AfterViewInit, OnInit {
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatTable) table: MatTable<Product>;
+  dataSource: ProductRead2DataSource;
+  constructor(private productService: ProductService){}
+  /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
+  products: Product[] = [];
+  displayedColumns = ['id', 'name', 'price'];
+
+  async ngOnInit() {
+    this.products = await this.productService.read();
+    this.products.reverse();
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+    this.table.dataSource = this.dataSource;
+  }
+}
